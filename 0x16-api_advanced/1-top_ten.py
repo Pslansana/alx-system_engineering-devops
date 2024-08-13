0e8950384019945cbc9fb2887f2226c3d032fd41
Peter Sylvester Lansana <Lansanapeters@gmail.com>
2024-08-13 15:22:15 -0700

    reddit api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""Contains top_ten function"""
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
 
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)"
-    }
+    headers = {"User-Agent": "Python/requests"}
     params = {
         "limit": 10
     }
